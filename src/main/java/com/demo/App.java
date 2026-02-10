5870a6ac8024afde0d00639b2b36b53971e6f087
Ajay <ajjurathod1998@gmail.com>
2026-02-10 09:43:25 +0000

    1st commit to mian branch

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,32 @@
+package com.demo;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port 8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello from GitHub Actions Docker App!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
+
